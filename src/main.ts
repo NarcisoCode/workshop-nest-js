15d279374584fba49dd814a1256d478aa714d20f
NarcisoCode <narcisocode@gmail.com>
2020-08-23 09:41:46 -0600

    log server

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  Logger.log(`Server is running in ${ await app.getUrl() }`)
 }
 bootstrap();
